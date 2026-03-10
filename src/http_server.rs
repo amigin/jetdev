51ab1a12e8dc77407f377d014ec0cd864a69b1a8
amigin <amigin@gmail.com>
2026-03-10 12:05:08 +0200

    New version of the web site

--- src/http_server.rs
+++ src/http_server.rs
@@ -5,12 +5,13 @@ use my_http_server::MyHttpServer;
 use crate::app_ctx::AppContext;
 
 pub fn start(app: &AppContext) {
-    let mut http_server = MyHttpServer::new(SocketAddr::from(([0, 0, 0, 0], 8080)));
+    let mut http_server = MyHttpServer::new(SocketAddr::from(([0, 0, 0, 0], 8082)));
 
-    http_server.add_middleware(Arc::new(my_http_server::StaticFilesMiddleware::new(
-        None,
-        Some(vec!["Index.html".to_string()]),
-    )));
+    let static_files = my_http_server::StaticFilesMiddleware::new(
+        
+    ).add_index_file("Index.html");
+
+    http_server.add_middleware(Arc::new(static_files));
 
     http_server.start(app.states.clone(), my_logger::LOGGER.clone());
 }
